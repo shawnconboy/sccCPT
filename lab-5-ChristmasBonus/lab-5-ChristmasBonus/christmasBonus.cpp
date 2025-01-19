a03697abec614ad79532b9a75ba49061cdb38379
Shawn Conboy <conboysbusiness@gmail.com>
2025-01-19 09:03:06 -0500

    Init files

---
+++ lab-5-ChristmasBonus/lab-5-ChristmasBonus/christmasBonus.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+	system("color f0");
+
+
+
+	//Declare Variables
+	float annualSalary = 0.00;
+	float yearsWorked = 0;
+	float bonusPercentage = 0.00;
+
+
+	//Process
+	cout << "Please enter your gross pay per year : ";
+	cin >> annualSalary;
+
+	cout << "Please enter number of years you worked : ";
+	cin >> yearsWorked;
+
+	if (yearsWorked <= 5) {
+		bonusPercentage = 0.01;
+	}
+	else if (yearsWorked >= 6) {
+		bonusPercentage = 0.02;
+	}
+
+	float bonus = annualSalary * bonusPercentage;
+
+
+	cout << "Your bonus is : $" << bonus << endl;
+
+	cout << "     T H A N K Y O U     ";
+
+
+
+
+	return 0;
+}
