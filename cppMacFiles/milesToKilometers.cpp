c1a5833d852b97e13dd8e6db78544f4917c5ac8b
Shawn Conboy <conboysbusiness@gmail.com>
2025-01-20 21:48:53 -0500

    Complete code for program to run

--- cppMacFiles/milesToKilometers.cpp
+++ cppMacFiles/milesToKilometers.cpp
@@ -4,7 +4,19 @@ using namespace std;
 int main() {
 
     //Declare Variables
-    
+    float milesInput;
+    const float KILOMETERS_IN_MILE = 1.60934;
+    float convertedNumber;
+
+    //Process
+    cout << "Enter number of miles : ";
+
+    cin >> milesInput;
+
+    //Math to convert mile to kilometer
+    convertedNumber = milesInput * KILOMETERS_IN_MILE;
+
+    cout << "There are " << convertedNumber << " kilometers in " << milesInput << " mile(s).";
 
 
     return 0;
