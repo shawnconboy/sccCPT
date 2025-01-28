4248b6aa2afcfd105440c6dcecf67db21776b65e
Shawn Conboy <conboysbusiness@gmail.com>
2025-01-27 22:42:30 -0500

    init files

---
+++ ch5num2TryThis/ch5num2TryThis/num6.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//
+//int main() {
+//	
+//	//declare variables
+//	double raiseAmount = 0.00;
+//	double salary = 0.00;
+//	double raise = 0.00;
+//	char department = ' ';
+//
+//	cout << "Hello. Please enter your salary : ";
+//	cin >> salary;
+//
+//	cout << "Please enter your department : ";
+//	cin >> department;
+//
+//	if (toupper(department) == 'A' || toupper(department) == 'B') {
+//		raise = 0.02;
+//		raiseAmount = salary * raise;
+//	}
+//	else if (toupper(department) == 'C') {
+//		raise = 0.015;
+//		raiseAmount = salary * raise;
+//	}
+//
+//	cout << "Congratulations. You're getting a $" << raiseAmount << " raise!";
+//
+//	return 0;
+//}
