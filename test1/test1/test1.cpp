7d8e050f2a03dcc4a995c0555388785c89d2a9cc
Shawn Conboy <conboysbusiness@gmail.com>
2025-01-31 13:30:34 -0500

    init files

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// declare variables
+	float taxRate = 0.00;
+	int income = 0;
+	int age = 0;
+
+	// output - input
+	cout << "Hello, please enter your income : ";
+	cin >> income;
+
+	// process
+	if (income >= 200000) {
+		taxRate = 0.20;
+	}
+	else {
+		cout << "Please enter your age : ";
+		cin >> age;
+
+		if (age >= 60) {
+			taxRate = 0.06;
+		}
+		else {
+			taxRate = 0.12;
+		}
+	}
+
+	cout << "Your tax amount is : " << taxRate << ". Thank you!" << endl;
+
+	return 0;
+}
