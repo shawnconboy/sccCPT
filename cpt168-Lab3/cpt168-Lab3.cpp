c7766d71126a03ec029b7e16a23fea450ad7f58b
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-07 01:18:43 -0500

    init files

--- cpt168-Lab3/cpt168-Lab3.cpp
+++ cpt168-Lab3/cpt168-Lab3.cpp
@@ -1,21 +1,48 @@
-// Shawn-Conboy-CPT-168-Lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Shawn-Conboy-CPT-168-A01H-Lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 using namespace std;
+
 int main() // Main Function
 {
 	system("color f0");
 
 	// Display My Information
-	cout << "\t\t**********************" << endl;
-	cout << "\t\t     Shawn Conboy     " << endl;
-	cout << "\t\t     CPT-168-A01H     " << endl;
-	cout << "\t\t  Calculate Commission" << endl;
-	cout << "\t\t        Lab-3         " << endl;
+	cout << "\t\t*************************" << endl;
+	cout << "\t\t*      Shawn Conboy     *" << endl;
+	cout << "\t\t*      CPT-168-AO1H     *" << endl;
+	cout << "\t\t*  Calculate Commission *" << endl;
+	cout << "\t\t*          Lab 3        *" << endl;
+	cout << "\t\t*************************" << endl;
 
+	// Declare_variables
+	double sale1			= 0.0;
+	double sale2			= 0.0;
+	double commissionRate	= 0.0;
+	double totalSales		= 0.0;
+	double commission		= 0.0;
 
-}
+	// Input
+
+	cout << "First sale? ";
+	cin >> sale1;
+	cout << "Second sale? ";
+	cin >> sale2;
+	cout << "Commission rate in decimal format? ";
+	cin >> commissionRate;
+
+	// Process
+	totalSales = sale1 + sale2;
+	commission = totalSales * commissionRate;
+
+	// Output
+	cout << "\tCommission: $" << commission << endl;
+	cout << "\n********** T H A N K Y O U **********\n" << endl;
+
+	system("pause");
+	return 0;
+}	//end of main function
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
