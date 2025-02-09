b1217cc6981a7f064961fde8ce38bcde579f1cae
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-09 15:13:11 -0500

    init files

---
+++ S-Conboy-Lab-4-Calculate-Auto-Commission/S-Conboy-Lab-4-Calculate-Auto-Commission.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	system("color f0");
+
+	// Display My Information
+	cout << "\t\t*************************" << endl;
+	cout << "\t\t*      Shawn Conboy     *" << endl;
+	cout << "\t\t*      CPT-168-AO1H     *" << endl;
+	cout << "\t\t*  Calculate Commission *" << endl;
+	cout << "\t\t*          Lab 3        *" << endl;
+	cout << "\t\t*************************" << endl;
+
+	//declare variables
+	int carsSold = 0;
+	double commission = 0.0;
+
+	//output-input
+	cout << "Hello. Please enter number of cars sold : ";
+	cin >> carsSold;
+
+	//process
+	commission = carsSold * 500;
+
+	//output
+	cout << "Your commission is : " << commission << endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ chapter6/ch6-16/ch6-16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//declare variables
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+
+	//output-input
+	cout << "Please enter your first number : ";
+	cin >> num1;
+
+	cout << "Please enter your first number : ";
+	cin >> num1;
+
+	cout << "Please enter your first number : ";
+	cin >> num1;
+	return 0;
+}
