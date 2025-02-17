066580662cc35e0883df39d7e94c6a13b7c3df76
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-17 00:59:20 -0500

    init file

---
+++ chapter7/miniQuiz7-2/miniQuiz7-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Write a C++ while clause that processes the loop body instructions as long as the value in the ordered variable is greater than the number 100.
+		//		while (num > 100);
+
+	/*Write a C++ while clause that stops the loop when the value in the quantity variable is less than the number 0. (Hint: Change the loop exit condition to a looping condition.)
+				while (quantity < 0);*/
+
+	/*Write a C++ while clause that processes the loop body instructions as long as the value in the inStock variable is greater than the value in the reorder variable.
+				while (inStock > reorder);*/
+
+	/*Write a C++ while clause that processes the loop body instructions as long as the value in a char variable named letter is either Y or y.
+				while (letter == 'Y' || letter == 'y')*/
+
+	return 0;
+}
