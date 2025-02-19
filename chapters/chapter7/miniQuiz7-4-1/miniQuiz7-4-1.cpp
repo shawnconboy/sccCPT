94846cabf1d931f1fdc6c9bf9b8b20209a59cbe8
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-19 04:24:39 -0500

    Init file

---
+++ chapters/chapter7/miniQuiz7-4-1/miniQuiz7-4-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	system("color f0");
+
+	int evenNum = 2;
+
+	while (evenNum < 9) {
+		cout << evenNum << endl;
+		evenNum = evenNum + 2;
+	}
+
+	system("pause");
+	return 0;
+}
