036dbb551c16063373701da97bc7018f8ab56be5
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-18 19:30:06 -0500

    init file

---
+++ chapter7/loopExample1/loopExample1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 10; i += 1) {
+		//i is equal to one. 
+		//if i is less than or equal to 10,
+		//add 1 to i.
+		cout << i << endl;
+	}
+
+	system("pause");
+}
