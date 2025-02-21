edec00bf70f9b26936bbf4298443ff8b9ce2ee3a
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-21 11:49:25 -0500

    consolidated code

--- chapters/chapter7/sumOfIntegers/sumOfIntegers.cpp
+++ chapters/chapter7/sumOfIntegers/sumOfIntegers.cpp
@@ -7,38 +7,30 @@ int main() {
 
 	cout << "\t***************************************" << endl;
 	cout << "\t*                                     *" << endl;
-	cout << "\t*           Sum of Intergers          *" << endl;
+	cout << "\t*           Sum of Integers           *" << endl;
 	cout << "\t*                                     *" << endl;
 	cout << "\t***************************************" << endl;
 
-	int num = 0;
-	int total = 0;
+	int num = 0, total = 0, counter = 0;
 	char showAverage = ' ';
 	double average = 0;
-	int counter = 0;
 
-	cout << "\n\tPlease enter a number : ";
-	cin >> num;
-	cout << endl;
+	cout << "\n\tPlease enter a number : ", cin >> num, cout << endl;
 
 	while (num > -1) {
 		total += num;
 		counter += 1;
-		cout << "\tPlease enter a new number : ";
-		cin >> num;
-		cout << endl;
+		cout << "\tPlease enter a new number : ", cin >> num , cout << endl;
 	}
 
 	cout << "\tFinal Total : " << total << endl << endl;
 
-	cout << "\tWould you like to know the average? (Y or N) : ";
-	cin >> showAverage;
+	cout << "\tWould you like to know the average? (Y or N) : ", cin >> showAverage;
 	showAverage = toupper(showAverage);
-	cout << endl;
 
 	if (showAverage == 'Y') {
 		average = static_cast<double>(total) / counter;
-		cout << "\tThe average is : " << fixed << setprecision(2) << average << endl << endl;
+		cout << "\n\tThe average is : " << fixed << setprecision(2) << average << endl << endl;
 	}
 
 	system("pause");
