dd36e34b317298ce723a2e4d912496c3e09034d2
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-25 21:51:11 -0500

    init file

---
+++ cppMacFiles/labs/lab7/source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    system("clear");
+
+    // display my info
+    cout << "\n\t\t****************************************" << endl;
+    cout << "\t\t*                                      *" << endl;
+    cout << "\t\t*         Square-Cube-Program          *" << endl;
+    cout << "\t\t*                                      *" << endl;
+    cout << "\t\t****************************************" << endl;
+
+    // declare variables
+    int userNum = 0;
+
+    // output input
+    cout << "Please enter a number to square, cube, and raise to the 4th power : ";
+    cin >> userNum;
+
+    // process
+
+    for (int i = 1; i < 11; i++)
+    {
+        cout << i << endl;
+    }
+
+    // final output
+}
