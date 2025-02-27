0744c893b660e9fc65e10214ca8f89887e713ff0
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-26 22:43:51 -0500

    init files

---
+++ miscProjects/nbaStatAnalysis/nbaStatAnalysis.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	cout << "**********************" << endl;
+	cout << "*                    *" << endl;
+	cout << "*  NBA Stat Tracker  *" << endl;
+	cout << "*                    *" << endl;
+	cout << "**********************" << endl;
+
+	// declare variables
+
+
+	//output input 
+
+
+
+
+	return 0;
+}
