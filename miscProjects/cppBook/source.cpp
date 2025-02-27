0be73cdc16c658dfaa2f4e4f9410fe5cdb6eb81e
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-26 20:59:32 -0500

    init file

---
+++ miscProjects/cppBook/source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// int main()
+// {
+//     system("clear");
+
+//     float height = 0.00;
+//     float width = 0.00;
+//     float area = 0.00;
+
+//     cout << "Please enter width" << endl;
+//     cin >> width;
+//     cout << "Please enter height" << endl;
+//     cin >> height;
+
+//     area = 0.5 * height * width;
+
+//     cout << "Area : " << area << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+
+//     system("clear");
+
+//     double x1, x2, y1, y2, m;
+
+//     cout << "Enter the first point : ";
+//     cin >> x1 >> x2;
+
+//     cout << "Enter the second point : ";
+//     cin >> y1 >> y2;
+
+//     m = (y2 - x2) / (y1 - x1);
+
+//     cout << "Slope : " << fixed << setprecision(2) << m << endl;
+
+//     return 0;
+// }
+
+int main()
+{
+
+    system("clear");
+    int a = 10, b;
+    b = a += 1;
+    cout << "Value of b is : " << b;
+
+    return 0;
+}
