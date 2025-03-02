de8f665e43331228ec5dab9a2ec971455851a6f7
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-01 22:48:28 -0500

    finished program

--- chapters/chapter7/ch7-26/source.cpp
+++ chapters/chapter7/ch7-26/source.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    system("clear");
 
     // display info
 
@@ -10,15 +11,27 @@ int main()
     cout << "*                      *" << endl;
     cout << "*   Fibonacci Display  *" << endl;
     cout << "*                      *" << endl;
-    cout << "************************" << endl;
+    cout << "************************\n"
+         << endl;
 
     // declare variables
-
+    int num1 = 1;
+    int num2 = 1;
+    int next = 0;
     // output input
 
+    cout << num1 << ", " << num2;
+
     // process
-
+    for (int i = 3; i < 11; i++)
+    {
+        next = num1 + num2;
+        cout << ", " << next;
+        num1 = num2;
+        num2 = next;
+    }
     // final output
-
+    cout << endl
+         << endl;
     return 0;
 }
