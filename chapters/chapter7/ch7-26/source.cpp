c0cfdbe2ece40e945b3ec92093251dc5d8061507
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-01 22:00:01 -0500

    init files

---
+++ chapters/chapter7/ch7-26/source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // display info
+
+    cout << "************************" << endl;
+    cout << "*                      *" << endl;
+    cout << "*   Fibonacci Display  *" << endl;
+    cout << "*                      *" << endl;
+    cout << "************************" << endl;
+
+    // declare variables
+
+    // output input
+
+    // process
+
+    // final output
+
+    return 0;
+}
