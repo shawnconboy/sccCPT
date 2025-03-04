c74c9e26cdc94494a941a87649e855b04ba424f4
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-04 11:55:58 -0500

    init files

---
+++ cppMacFiles/chapters/chapter7/exampleWhileLoop/source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // declare variables
+
+    double sales = 0.0;
+    double commission = 0.0;
+
+    // output input
+    cout << "Hey, how much did you make? : ";
+    cin >> sales;
+
+    // process
+    while (sales != -1)
+    {
+        commission = sales * 0.2;
+        cout << "Commission = : " << commission << endl;
+
+        cout << "Hey, how much did you make? : ";
+        cin >> sales;
+    }
+
+    return 0;
+}
