8a73023c03a8bdbdcbb0615200ad4a8dfc749488
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-05 10:49:49 -0500

    init files

---
+++ cppMacFiles/chapters/chapter8/ch8-14/source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    system("clear");
+    // display information
+
+    // declare variables
+
+    // output input
+
+    for (int row = 1; row < 5; row++)
+    {
+        cout << row << endl;
+    }
+
+    // process
+
+    // final output
+}
