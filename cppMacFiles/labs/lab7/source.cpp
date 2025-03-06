78f6c9faf3cde42380e0b4ca70ca7cf6a70def55
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-06 18:43:28 -0500

    init files

--- cppMacFiles/labs/lab7/source.cpp
+++ cppMacFiles/labs/lab7/source.cpp
@@ -14,18 +14,35 @@ int main()
     cout << "\t\t****************************************" << endl;
 
     // declare variables
+
     int userNum = 0;
+    char runAgain = ' ';
 
     // output input
-    cout << "Please enter a number to square, cube, and raise to the 4th power : ";
-    cin >> userNum;
 
-    // process
-
-    for (int i = 1; i < 11; i++)
+    do
     {
-        cout << i << endl;
-    }
+
+        cout << "\nPlease enter a number to square, cube, and raise to the 4th power : ";
+        cin >> userNum;
+
+        // process
+
+        cout << "Number     Square    Cube      4th Power" << endl;
+        cout << "--------   ------    ------    -----------" << endl;
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            cout << left << setw(11) << userNum << setw(10) << pow(userNum, 2.0) << setw(10) << pow(userNum, 3.0) << setw(15) << pow(userNum, 4.0) << endl;
+
+            userNum += 5;
+        }
+
+        cout << "Would you like to continue (Y or N)? ";
+        cin >> runAgain;
+        runAgain = toupper(runAgain);
+    } while (runAgain != 'N');
 
     // final output
 }
