84bebfb4ba3e4caba69206b9cafd0476bd5fe227
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-07 17:29:27 -0500

    init files

--- cppMacFiles/chapters/chapter8/ch8-9/secondTry/source.cpp
+++ cppMacFiles/chapters/chapter8/ch8-9/secondTry/source.cpp
@@ -22,22 +22,49 @@ using namespace std;
 //     return 0;
 // }
 
+// int main()
+// {
+//     system("clear");
+//     int numRows = 0;
+
+//     cout << "Please enter number of rows: ";
+//     cin >> numRows;
+
+//     for (int row = numRows; row >= 1; row--) // Start from numRows and decrease
+//     {
+//         for (int stars = 1; stars <= row; stars++) // Print 'row' number of stars
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
 int main()
 {
-    system("clear");
-    int numRows = 0;
 
-    cout << "Please enter number of rows: ";
-    cin >> numRows;
+    int userNum = 0;
 
-    for (int row = numRows; row >= 1; row--) // Start from numRows and decrease
+    cout << "Please enter a number : ";
+    cin >> userNum;
+
+    do
     {
-        for (int stars = 1; stars <= row; stars++) // Print 'row' number of stars
+        cout << userNum;
+
+        if (userNum % 2)
         {
-            cout << "* ";
+            cout << " -- Even";
         }
+        else
+        {
+            cout << " -- Odd";
+        }
+        userNum--;
         cout << endl;
-    }
+    } while (userNum > 0);
 
     return 0;
 }
