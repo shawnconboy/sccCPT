c36e5cae36fede3d3baa7031d72c7af5ddf97b6b
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-06 19:21:38 -0500

    passed to windows

--- CPT-168-Labs/S-Conboy-Lab-7/S-Conboy-Lab-7.cpp
+++ CPT-168-Labs/S-Conboy-Lab-7/S-Conboy-Lab-7.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
+
+// Function to clear the screen based on OS
+void clearScreen()
+{
+#ifdef _WIN32
+	system("CLS");
+#else
+	system("clear");
+#endif
+}
 
 int main()
 {
-	system("clear");
+	clearScreen();
 
 	// display my info
 	cout << "\n\t\t****************************************" << endl;
@@ -15,28 +24,36 @@ int main()
 	cout << "\t\t****************************************" << endl;
 
 	// declare variables
+
 	int userNum = 0;
-	double numSquared = 0;
+	char runAgain = ' ';
 
 	// output input
-	cout << "Please enter a number to square, cube, and raise to the 4th power : ";
-	cin >> userNum;
 
-	// process
-
-	//toSquare
-	numSquared = pow(userNum, 2.0);
-
-	cout << numSquared << endl;
-
-
-
-	for (int i = 1; i < 11; i++)
+	do
 	{
-		cout << numSquared;
-	}
+		cout << "\nPlease enter a number to square, cube, and raise to the 4th power : ";
+		cin >> userNum;
+
+		// process
+
+		cout << "Number     Square    Cube      4th Power" << endl;
+		cout << "--------   ------    ------    -----------" << endl;
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			cout << left << setw(11) << userNum << setw(10) << pow(userNum, 2.0) << setw(10) << pow(userNum, 3.0) << setw(15) << pow(userNum, 4.0) << endl;
+
+			userNum += 5;
+		}
+
+		cout << "Would you like to continue (Y or N)? ";
+		cin >> runAgain;
+		runAgain = toupper(runAgain);
+
+		clearScreen();
+	} while (runAgain != 'N');
 
 	// final output
-
-	return 0;
 }
