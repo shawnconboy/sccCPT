a81394fbba9fd9601f3de60e30c4b35640ef8545
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-12 00:57:23 -0400

    init files

---
+++ cppMacFiles/chapters/chapter9/cmathPractice/source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    system("clear");
+
+    // // 5 to the 2nd power
+    // cout << pow(5.0, 2) << endl;
+
+    // // 2 to the 4th power
+    // cout << pow(2, 4) << endl;
+
+    cout << sqrt(25) << endl;
+    cout << sqrt(400) << endl;
+
+    return 0;
+}
