c27ec4feadbafb45d54b636c346df3fc40147635
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-14 23:05:09 -0400

    Finesse code

--- cppMacFiles/chapters/chapter9/ch9-16/source.cpp
+++ cppMacFiles/chapters/chapter9/ch9-16/source.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 
+    cout << "Number   Square    Cube" << endl;
+
     for (int row = 10; row < 14; row++)
     {
-        cout << "Row : " << row << endl;
-
         for (int col = 0; col < 4; col++)
         {
-            cout << row << "    ";
-            cout << pow(row, 2) << "    ";
+            cout << "  " << row << "      ";
+            cout << pow(row, 2) << "      ";
             cout << pow(row, 3) << "    " << endl;
 
             row++;
