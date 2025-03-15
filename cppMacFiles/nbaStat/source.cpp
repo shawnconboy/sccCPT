8dd5811a9ab54f436cb607b6ac25c66fb0843b2a
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-14 20:22:40 -0400

    Add functions for rebounds and assists

--- cppMacFiles/nbaStat/source.cpp
+++ cppMacFiles/nbaStat/source.cpp
@@ -3,27 +3,64 @@
 using namespace std;
 
 void getPoints();
+void getRebounds();
+void getAssists();
 
 int main()
 {
 
     int option = 0;
+    char runAgain = ' ';
 
     system("clear");
 
-    cout << "\n**********************" << endl;
-    cout << "*                    *" << endl;
-    cout << "*  NBA Stat Tracker  *" << endl;
-    cout << "*                    *" << endl;
-    cout << "**********************" << endl;
+    do
+    {
+        system("clear");
 
-    cout << "\nPlease pick an option." << endl;
+        cout << "\n**********************" << endl;
+        cout << "*                    *" << endl;
+        cout << "*  NBA Stat Tracker  *" << endl;
+        cout << "*                    *" << endl;
+        cout << "**********************" << endl;
 
-    cout << "1 -- Points" << endl;
-    cout << "2 -- Rebounds" << endl;
-    cout << "3 -- Assists" << endl;
+        cout << "\nPlease pick an option." << endl;
 
-    cin >> option;
+        cout << "1 -- Points" << endl;
+        cout << "2 -- Rebounds" << endl;
+        cout << "3 -- Assists" << endl;
+        cout << "4 -- All" << endl
+             << endl;
+
+        cout << "Option : ";
+        cin >> option;
+
+        if (option < 0 || option > 4)
+        {
+            cout << "Invalid Option" << endl;
+        }
+        else if (option == 1)
+        {
+            getPoints();
+        }
+        else if (option == 2)
+        {
+            getRebounds();
+        }
+        else if (option == 3)
+        {
+            getAssists();
+        }
+        else if (option == 4)
+        {
+            getRebounds();
+        }
+
+        cout << "Would you like to run again? : ";
+        cin >> runAgain;
+        runAgain = toupper(runAgain);
+
+    } while (runAgain == 'Y');
 
     return 0;
 }
@@ -43,5 +80,39 @@ void getPoints()
     }
     pointAverage = totalPoints / 5;
     cout << "Point Average : " << pointAverage << endl
+         << endl;
+}
+
+void getRebounds()
+{
+
+    int rebounds = 0;
+    int totalRebounds = 0;
+    double reboundAverage = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Please enter number of rebounds : ";
+        cin >> rebounds;
+        totalRebounds += rebounds;
+    }
+    reboundAverage = totalRebounds / 5;
+    cout << "Rebound Average : " << reboundAverage << endl
+         << endl;
+}
+
+void getAssists()
+{
+
+    int assists = 0;
+    int totalAssists = 0;
+    double assistAverage = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Please enter number of assists : ";
+        cin >> assists;
+        totalAssists += assists;
+    }
+    assistAverage = totalAssists / 5;
+    cout << "Assist Average : " << assistAverage << endl
          << endl;
 }
