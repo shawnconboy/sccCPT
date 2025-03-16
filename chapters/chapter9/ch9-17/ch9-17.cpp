dd5cbb2bdc3eb51ec5943eb9f862e12fbb55f7ee
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-15 23:35:12 -0400

    Update files

---
+++ chapters/chapter9/ch9-17/ch9-17.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Function to clear the screen based on OS
+void clearScreen()
+{
+#ifdef _WIN32
+	system("CLS");
+#else
+	system("clear");
+#endif
+}
+
+int main()
+{
+	clearScreen();
+
+	int hoursWorked = 0;
+	int hourlyRate = 0;
+	double gross = 0;
+	char runAgain = ' ';
+
+	do
+	{
+		cout << "Please enter hours worked : ";
+		cin >> hoursWorked;
+
+		cout << "Please enter hourly rate : ";
+		cin >> hourlyRate;
+
+		if (hoursWorked <= 37)
+		{
+			gross = hoursWorked * hourlyRate;
+		}
+		else if (hoursWorked <= 50) // Overtime (1.5x)
+		{
+			gross = (37 * hourlyRate) + ((hoursWorked - 37) * hourlyRate * 1.5);
+		}
+		else // Double time (2x)
+		{
+			gross = (37 * hourlyRate) + (13 * hourlyRate * 1.5) + ((hoursWorked - 50) * hourlyRate * 2);
+		}
+
+		cout << "Gross pay : $" << gross << endl;
+
+		cout << "Enter another employee (Y or N) : ";
+		cin >> runAgain;
+	} while (toupper(runAgain) == 'Y');
+
+	cout << "Program ended" << endl;
+	return 0;
+}
