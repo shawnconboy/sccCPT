7e9a4249bce5fdce944d63ef03935fcb2c2a696a
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-17 17:28:37 -0400

    init files

---
+++ cppMacFiles/chapters/chapter9/ch9-24/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int number = 1;
+
+    system("clear");
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i + 1 << endl;
+    }
+
+    return 0;
+}
