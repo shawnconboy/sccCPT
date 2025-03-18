4cb00cd8f4a08ba42ed8fb129af80b32da33f4a2
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-18 17:00:14 -0400

    init files

---
+++ cppMacFiles/chapters/chapter11/ch11-4/source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("clear");
+
+    double rates[5] = {6.5, 8.3, 4.0, 2.0, 10.5};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << rates[i] << endl;
+    }
+
+    return 0;
+}
