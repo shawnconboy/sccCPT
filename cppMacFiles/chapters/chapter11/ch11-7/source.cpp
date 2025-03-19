ee11b666048228a41a17288fc53eb36caad8d0c8
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-19 00:21:38 -0400

    init files

---
+++ cppMacFiles/chapters/chapter11/ch11-7/source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    system("clear");
+    bool answers[5] = {};
+
+    answers[2] = true;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << answers[i] << endl;
+    }
+
+    return 0;
+}
---
+++ cppMacFiles/chapters/chapter11/ch11-8/source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int nums[5] = {10, 15, 20, 25, 30};
+
+    int numDoubled = nums[0] * 2;
+
+    cout << numDoubled << endl;
+
+    int num1AddNum2 = nums[0] + nums[1];
+
+    cout << num1AddNum2 << endl;
+
+    return 0;
+}
