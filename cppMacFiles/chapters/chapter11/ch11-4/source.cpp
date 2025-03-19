d8e42245a0b5c000d1fa93087cba7381494178e9
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-19 00:06:20 -0400

    init files

--- cppMacFiles/chapters/chapter11/ch11-4/source.cpp
+++ cppMacFiles/chapters/chapter11/ch11-4/source.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+
+    int counter = 0;
+
     system("clear");
 
     double rates[5] = {6.5, 8.3, 4.0, 2.0, 10.5};
@@ -11,6 +14,14 @@ int main()
     {
         cout << rates[i] << endl;
     }
+
+    cout << endl;
+
+    do
+    {
+        cout << rates[counter] << endl;
+        counter++;
+    } while (counter < 5);
 
     return 0;
 }
