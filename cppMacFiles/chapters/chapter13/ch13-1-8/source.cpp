36828622eefb8f9e55da3e77e4b64e7fd00d0aec
Shawn Conboy <conboysbusiness@gmail.com>
2025-03-31 00:30:38 -0400

    init files

---
+++ cppMacFiles/chapters/chapter13/ch13-1-8/source.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int main()
+// {
+//     system("clear");
+
+//     int numChars = 0;
+//     string message = "Gitches";
+
+//     // cout << "Please enter a message: " << endl;
+//     // getline(cin, message);
+
+//     // cout << message.length() << endl;
+//     // // this prints the element length
+
+//     // cout << message.erase(0, 2) << endl;
+//     // // this prints your message, minus the first two characters
+
+//     // cout << message.replace(0, 2, "   ") << endl;
+//     // // this does the same thing as the message above.
+
+//     cout << message.replace(0, 1, "B") << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+
+//     string address = "1320 Lawson Blvd.";
+//     string streetNum = "";
+
+//     streetNum = address.substr(0, 4);
+
+//     cout << streetNum << endl;
+//     cout << address << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     system("clear");
+
+//     string part = "ABCD34G";
+//     string code = "";
+
+//     code = part.substr(4, 2);
+
+//     cout << code << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     system("clear");
+
+//     string word = "mend";
+
+//     word.insert(0, "A");
+//     cout << word << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     system("clear");
+
+//     string word = "mouse";
+
+//     word.replace(0, 5, "mouth");
+
+//     cout << word << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+// system("clear");
+//     return 0;
+// }
+
+// int main()
+// {
+// system("clear");
+//     return 0;
+// }// int main()
+// {
+// system("clear");
+//     return 0;
+// }
