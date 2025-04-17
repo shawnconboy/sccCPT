f1d7ceb995b36e4abe0bc854d62ec69e252071a9
Shawn Conboy <conboysbusiness@gmail.com>
2025-04-17 06:03:02 -0400

    init files

--- cppMacFiles/labs/lab10/source.cpp
+++ cppMacFiles/labs/lab10/source.cpp
@@ -9,18 +9,67 @@ int main()
 
     system("clear");
 
-    ofstream outPayroll;
+    // *************************************************
 
-    outPayroll.open("payroll.txt", ios::app);
+    // ofstream outPayroll;
 
-    if (outPayroll.is_open())
-    {
-        cout << "payroll.txt opened successfully." << endl;
-    }
-    else
-    {
-        cout << "payroll.txt not found." << endl;
-    }
+    // outPayroll.open("payroll.txt", ios::app);
+
+    // if (outPayroll.is_open())
+    // {
+    //     cout << "payroll.txt opened successfully." << endl;
+    // }
+    // else
+    // {
+    //     cout << "payroll.txt not found." << endl;
+    // }
+
+    // *************************************************
+
+    // ofstream outJan;
+
+    // outJan.open("janSales.txt", ios::app);
+
+    // if (outJan.is_open())
+    // {
+    //     cout << "Success" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Failed to open" << endl;
+    // }
+
+    // *************************************************
+
+    // ifstream inInventory;
+
+    // inInventory.open("inventory.txt");
+
+    // if (inInventory.is_open())
+    // {
+    //     cout << "success" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Failed" << endl;
+    // }
+
+    // *************************************************
+
+    // ofstream outSales;
+
+    // outSales.open("firstQtr.txt", ios::app);
+
+    // if (outSales.is_open())
+    // {
+    //     cout << "Success" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Failure" << endl;
+    // }
+
+    // *************************************************
 
     return 0;
 }
