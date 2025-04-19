f7462d4fd8c9ae0c128419c508c2deb0444fc04e
Shawn Conboy <conboysbusiness@gmail.com>
2025-04-19 00:24:12 -0400

    push

--- cppMacFiles/labs/lab10/source.cpp
+++ cppMacFiles/labs/lab10/source.cpp
@@ -4,8 +4,93 @@
 
 using namespace std;
 
+// object declarations
+ofstream outPayroll;
+ifstream inPayroll;
+
+string name = "";
+string ssn = "";
+int hours = 0;
+double rate = 0.0;
+double gross = 0.0;
+double deductions = 0.0;
+double net = 0.0;
+
 int main()
 {
+
+    system("clear");
+
+    outPayroll.open("payroll.txt", ios::app);
+    if (outPayroll.is_open())
+    {
+        cout << "payroll.txt opened successfully" << endl;
+    }
+    else
+    {
+        cout << "payroll.txt failed to open" << endl;
+    }
+
+    cout << "___________________________________________" << endl
+         << endl;
+
+    outPayroll.close();
+
+    if (!outPayroll.is_open())
+    {
+        cout << "payroll.txt closed successfully" << endl;
+    }
+    else
+    {
+        cout << "payroll.txt failed to close" << endl;
+    }
+
+    cout << "___________________________________________" << endl
+         << endl;
+
+    inPayroll.open("payroll.txt");
+
+    if (inPayroll.is_open())
+    {
+        cout << "payroll.txt opened successfully" << endl;
+    }
+    else
+    {
+        cout << "payroll.txt failed to open" << endl;
+    }
+
+    cout << "___________________________________________" << endl
+         << endl;
+
+    string hoursString = "";
+    string rateString = "";
+
+    cout << fixed << setprecision(2);
+
+    cout << "SSN\t\t" << "Name\t\t\t" << "Hours\t" << "Rate\t" << "Gross\t\t" << "Deductions\t" << "NetPay" << endl;
+    cout << "___\t\t" << "____\t\t\t" << "_____\t" << "____\t" << "_____\t\t" << "__________\t" << "______" << endl
+         << endl;
+
+    while (getline(inPayroll, name, '#') && getline(inPayroll, ssn, '#') && getline(inPayroll, hoursString, '#') && getline(inPayroll, rateString))
+    {
+        hours = stoi(hoursString);
+        rate = stod(rateString);
+
+        if (hours > 40)
+        {
+            gross = 40 * rate + (hours - 40) * (rate * 1.5);
+        }
+        else
+        {
+            gross = hours * rate;
+        }
+
+        deductions = gross * 0.1;
+
+        net = gross - deductions;
+
+        cout << ssn << "\t" << name << "\t\t" << hours << "\t" << rate << "\t" << gross << "\t\t" << deductions << "\t\t" << net << endl;
+    }
 
     return 0;
 }
