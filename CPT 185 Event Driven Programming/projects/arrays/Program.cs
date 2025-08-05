efc96bbd07de6531ccf22b271dba3e6b7f31b072
Shawn Conboy <conboysbusiness@gmail.com>
2025-08-04 22:47:57 -0400

    update

---
+++ CPT 185 Event Driven Programming/projects/arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+
+}
--- CPT 185 Event Driven Programming/projects/arrays/Properties/AssemblyInfo.cs
+++ CPT 185 Event Driven Programming/projects/arrays/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("program")]
+[assembly: AssemblyTitle("arrays")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("program")]
+[assembly: AssemblyProduct("arrays")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f5cccd4-0c64-4878-9961-7e11c61e0fe3")]
+[assembly: Guid("2ecd25a9-ef73-4604-a842-d10f58cb81c1")]
 
 // Version information for an assembly consists of the following four values:
 //
