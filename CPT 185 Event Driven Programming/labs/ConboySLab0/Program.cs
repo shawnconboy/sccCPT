4b841d5fa16f18acb5f2ed89be8582259f3485c2
Shawn Conboy <conboysbusiness@gmail.com>
2025-08-21 21:13:06 -0400

    init

--- CPT 185 Event Driven Programming/labs/ConboySLab0/Program.cs
+++ CPT 185 Event Driven Programming/labs/ConboySLab0/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections.Specialized;
 
-
-
 // Cout is Console.WriteLine();
 // Cin is Console.ReadLine();
-
 
 // get age name and fav number
 
@@ -20,7 +17,6 @@
 
 //Console.Write("Please enter your favorite number : ");
 //favNumber = int.Parse(Console.ReadLine());
-
 
 //Console.WriteLine($"\nHello {firstName}, your are {age} years old. Your favorite number is {favNumber}.");
 //Console.WriteLine($"The difference between your age and favorite number is {(age - favNumber)}.");
@@ -40,31 +36,14 @@
 
 // *****************************************************************************************************************************
 
-// Declare Variables
+// Declare Variables / Functions
 
 void clearScreen()
 {
     Console.Clear();
 }
 
-int[] examScores = [0, 0, 0, 0];
-int examTotal = 0;
-
-int[] labScores = [0, 0, 0, 0, 0];
-int labTotal = 0;
-
-int[] quizScores = [0, 0, 0, 0, 0];
-int quizTotal = 0;
-
-char runAgain = 'y';
-
-string studentName = "";
-
-
-// loop per student
-while (runAgain == 'y')
-
-
+void header()
 {
     Console.WriteLine("***********************************************************");
     Console.WriteLine("*                                                         *");
@@ -73,11 +52,36 @@ while (runAgain == 'y')
     Console.WriteLine("*                       Lab 0                             *");
     Console.WriteLine("*                                                         *");
     Console.WriteLine("***********************************************************");
+    Console.WriteLine();
+}
+
+void transition()
+{
+    clearScreen();
+    header();
+}
+
+char runAgain = 'y';
+string studentName = "";
+
+// loop per student
+while (runAgain == 'y')
+{
+    transition();
+
+    int[] examScores = [0, 0, 0, 0];
+    int[] labScores = [0, 0, 0, 0, 0];
+    int[] quizScores = [0, 0, 0, 0, 0];
+    int examTotal = 0;
+    int labTotal = 0;
+    int quizTotal = 0;
 
     Console.WriteLine("Hello. Please enter your name.");
     studentName = Console.ReadLine();
     Console.WriteLine();
 
+    transition();
+    Console.WriteLine($"Hello, {studentName}.\n");
     // Loop to get exam scores
     for (int x = 0; x < 4; x++)
     {
@@ -85,7 +89,10 @@ while (runAgain == 'y')
         examScores[x] = int.Parse(Console.ReadLine());
         examTotal += examScores[x];
     }
+    Console.ReadLine();
 
+    transition();
+    Console.WriteLine($"Hello, {studentName}.\n");
     // Loop to get lab scores
     for (int x = 0; x < 5; x++)
     {
@@ -93,6 +100,10 @@ while (runAgain == 'y')
         labScores[x] = int.Parse(Console.ReadLine());
         labTotal += labScores[x];
     }
+    Console.ReadLine();
+
+    transition();
+    Console.WriteLine($"Hello, {studentName}.\n");
 
     // Loop to get quiz scores
     for (int x = 0; x < 5; x++)
@@ -102,16 +113,54 @@ while (runAgain == 'y')
         quizTotal += quizScores[x];
     }
 
+    // *****************************************************************************************************************************
+
+    transition();
+    Console.WriteLine($"Hello, {studentName}.\n");
+
+    // Loop to show individual exam scores
     for (int x = 0; x < examScores.Length; x++)
     {
         Console.WriteLine($"Exam {x + 1} score : {examScores[x]}");
     }
+    Console.ReadLine();
 
-    Console.WriteLine(examTotal);
+    // Loop to show individual lab scores
+    for (int x = 0; x < labScores.Length; x++)
+    {
+        Console.WriteLine($"Lab {x + 1} score : {labScores[x]}");
+    }
+    Console.ReadLine();
 
+    // Loop to show individual quiz scores
+    for (int x = 0; x < quizScores.Length; x++)
+    {
+        Console.WriteLine($"Quiz {x + 1} score : {quizScores[x]}");
+    }
+    Console.ReadLine();
+
+    // *****************************************************************************************************************************
+
+    double examAverage = (double)examTotal / examScores.Length;
+    double labAverage = (double)labTotal / labScores.Length;
+    double quizAverage = (double)quizTotal / quizScores.Length;
+
+    double weightedExam = examAverage * 0.45;
+    double weightedLab = labAverage * 0.35;
+    double weightedQuiz = quizAverage * 0.2;
+
+    Console.WriteLine($"Exam scores average : {examAverage}");
+    Console.WriteLine($"Lab scores average : {labAverage}");
+    Console.WriteLine($"Quiz scores average : {quizAverage}");
+
+    double finalAverage = weightedExam + weightedLab + weightedQuiz;
+
+    Console.WriteLine($"Your overall grade is {finalAverage}");
 
     Console.WriteLine("Would you like to check another student's grades? (Y/N)");
     runAgain = char.Parse(Console.ReadLine().ToLower());
 }
 
+transition();
 Console.WriteLine("Program has ended. Thank you.");
+
