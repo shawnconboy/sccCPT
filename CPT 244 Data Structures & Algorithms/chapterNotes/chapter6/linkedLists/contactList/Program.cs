a0255a6c73da7072b8595292527b535f1eeae672
Shawn Conboy <shawnconboy@Shawns-MacBook-Air.local>
2025-10-17 11:39:03 -0400

    Add a few practice projects for list

---
+++ CPT 244 Data Structures & Algorithms/chapterNotes/chapter6/linkedLists/contactList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+    }
+    static void Main()
+    {
+        Console.Clear();
+        List<Contact> contacts = new List<Contact>();
+
+        contacts.Add(new Contact("Brielle", "864-444-5555"));
+        contacts.Add(new Contact("Miley", "864-888-9988"));
+
+        foreach (Contact c in contacts)
+        {
+            Console.WriteLine($"{c.Name} - {c.Phone}");
+        }
+    }
+}
