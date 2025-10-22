0366fee447a5fa7ad191be6ba92ca0093548bf4a
Shawn Conboy <shawnconboy@Mac.attlocal.net>
2025-10-22 09:06:27 -0400

    String delimeter practice

---
+++ CPT 185 Event Driven Programming/chapterNotes/chapter8/question8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection.Metadata;
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Enter a string of numbers separated by commas.");
+
+        string userString = Console.ReadLine();
+
+        // tokenize and then add total
+
+        userString.Trim();
+
+        string[] token = userString.Split(',');
+
+        int[] nums = new int[token.Length];
+
+        int total = 0;
+        for (int i = 0; i < token.Length; i++)
+        {
+            nums[i] = int.Parse(token[i]);
+            total += nums[i];
+        }
+
+        Console.WriteLine("Total is : " + total.ToString());
+    }
+}
