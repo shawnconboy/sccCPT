700287cc1f3a3235dead0db108229d3999ab0e8f
Shawn Conboy <shawnconboy@Mac.attlocal.net>
2025-10-26 16:30:03 -0400

    Object practice

---
+++ CPT 244 Data Structures & Algorithms/chapterNotes/chapter6/car/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Garage
+{
+    class Battery
+    {
+        public bool charged = true;
+    }
+
+    class Starter
+    {
+        public bool Engage(Battery battery)
+        {
+            if (battery.charged)
+            {
+                Console.WriteLine("Starter Engaged.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Battery dead. Can't start.");
+                return false;
+            }
+        }
+    }
+
+    class Car
+    {
+        public Battery battery = new Battery();
+        public Starter starter = new Starter();
+
+        public void Start()
+        {
+            Console.WriteLine("Attempting to start car...");
+            bool started = starter.Engage(battery);
+
+            if (started)
+            {
+                Console.WriteLine("Car is running.");
+            }
+            else
+            {
+                Console.WriteLine("Car failed to start.");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Car myCar = new Car();
+
+            myCar.battery.charged = false;
+            myCar.Start();
+        }
+    }
+}
---
+++ CPT 244 Data Structures & Algorithms/chapterNotes/chapter6/linkedListTwo/sConboyLinkedListTwoApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Node
+{
+    // this will hold data
+    public int value;
+
+    // this will point to the next node in the list
+    public Node next;
+
+    // this is the constuctor, it makes a node with a given value
+    public Node(int value)
+    {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList
+{
+    // first node in the list
+    public Node head;
+
+    // add new node to end of list
+    public void Add(int newValue)
+    {
+        Node newNode = new Node(newValue);
+
+        // if list is empty, new node will become the head
+        if (head == null)
+        {
+            head = newNode;
+        }
+        // if the list is not empty, find the end
+        else
+        {
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            // attach the new node to the end
+            current.next = newNode;
+        }
+    }
+
+    // display all node values
+    public void PrintList()
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.Write(current.value + " ");
+            // move onto the next node
+            current = current.next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.Clear();
+        LinkedList myList = new LinkedList();
+
+        myList.Add(10);
+        myList.Add(20);
+        myList.Add(30);
+
+        myList.PrintList();
+    }
+}
