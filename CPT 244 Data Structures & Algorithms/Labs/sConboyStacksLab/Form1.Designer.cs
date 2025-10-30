1bc23c90de12834ad148a17e7a4627f627e6e45b
Shawn Conboy <conboysbusiness@gmail.com>
2025-10-29 22:54:52 -0400

    244 Stack Lab

--- CPT 244 Data Structures & Algorithms/Labs/sConboyStacksLab/Form1.Designer.cs
+++ CPT 244 Data Structures & Algorithms/Labs/sConboyStacksLab/Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            urlTextbox = new TextBox();
+            searchButton = new Button();
+            historyButton = new Button();
+            searchPicturebox = new PictureBox();
+            historyListbox = new ListBox();
+            backPicturebox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)searchPicturebox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)backPicturebox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(198, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(404, 124);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // urlTextbox
+            // 
+            urlTextbox.Location = new Point(227, 207);
+            urlTextbox.Name = "urlTextbox";
+            urlTextbox.PlaceholderText = "Search or type a URL";
+            urlTextbox.Size = new Size(348, 27);
+            urlTextbox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            searchButton.Cursor = Cursors.Hand;
+            searchButton.Location = new Point(198, 266);
+            searchButton.Name = "searchButton";
+            searchButton.Size = new Size(132, 29);
+            searchButton.TabIndex = 1;
+            searchButton.Text = "Google Search";
+            searchButton.UseVisualStyleBackColor = true;
+            searchButton.Click += searchPicturebox_Click;
+            // 
+            // historyButton
+            // 
+            historyButton.Cursor = Cursors.Hand;
+            historyButton.Location = new Point(470, 266);
+            historyButton.Name = "historyButton";
+            historyButton.Size = new Size(132, 29);
+            historyButton.TabIndex = 2;
+            historyButton.Text = "See History";
+            historyButton.UseVisualStyleBackColor = true;
+            historyButton.Click += historyButton_Click;
+            // 
+            // searchPicturebox
+            // 
+            searchPicturebox.Cursor = Cursors.Hand;
+            searchPicturebox.Image = (Image)resources.GetObject("searchPicturebox.Image");
+            searchPicturebox.Location = new Point(198, 209);
+            searchPicturebox.Name = "searchPicturebox";
+            searchPicturebox.Size = new Size(23, 23);
+            searchPicturebox.SizeMode = PictureBoxSizeMode.StretchImage;
+            searchPicturebox.TabIndex = 4;
+            searchPicturebox.TabStop = false;
+            searchPicturebox.Click += searchPicturebox_Click;
+            // 
+            // historyListbox
+            // 
+            historyListbox.FormattingEnabled = true;
+            historyListbox.Location = new Point(227, 316);
+            historyListbox.Name = "historyListbox";
+            historyListbox.Size = new Size(348, 64);
+            historyListbox.TabIndex = 3;
+            historyListbox.Visible = false;
+            // 
+            // backPicturebox
+            // 
+            backPicturebox.Cursor = Cursors.Hand;
+            backPicturebox.Image = (Image)resources.GetObject("backPicturebox.Image");
+            backPicturebox.Location = new Point(579, 209);
+            backPicturebox.Name = "backPicturebox";
+            backPicturebox.Size = new Size(23, 23);
+            backPicturebox.SizeMode = PictureBoxSizeMode.StretchImage;
+            backPicturebox.TabIndex = 6;
+            backPicturebox.TabStop = false;
+            backPicturebox.Click += backPicturebox_Click;
+            // 
+            // Form1
+            // 
+            AcceptButton = searchButton;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 426);
+            Controls.Add(backPicturebox);
+            Controls.Add(historyListbox);
+            Controls.Add(searchPicturebox);
+            Controls.Add(historyButton);
+            Controls.Add(searchButton);
+            Controls.Add(urlTextbox);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Google";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)searchPicturebox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)backPicturebox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox urlTextbox;
+        private Button searchButton;
+        private Button historyButton;
+        private PictureBox searchPicturebox;
+        private ListBox historyListbox;
+        private PictureBox backPicturebox;
     }
 }
--- CPT 244 Data Structures & Algorithms/Labs/sConboyStacksLab/Form1.cs
+++ CPT 244 Data Structures & Algorithms/Labs/sConboyStacksLab/Form1.cs
@@ -1,10 +1,63 @@
+using System.Security.Policy;
+
 namespace sConboyStacksLab
 {
     public partial class Form1 : Form
     {
+
+        // make a stack        
+        private Stack<string> siteHistory = new Stack<string>();
+
+        // declare some variables to be used throughout
+        string userURL = "";
+
         public Form1()
         {
             InitializeComponent();
+            urlTextbox.TabStop = false;
+            this.Shown += (s, e) => urlTextbox.TabStop = true;   // re-enable after shown
+        }
+
+
+        private void searchPicturebox_Click(object sender, EventArgs e)
+        {
+            // add site to stack
+            userURL = urlTextbox.Text;
+            siteHistory.Push(userURL);
+            urlTextbox.Clear();
+
+            // simulate visiting site
+            MessageBox.Show($"{siteHistory.Peek()} visited.");
+        }
+
+        private void historyButton_Click(object sender, EventArgs e)
+        {
+            // add sites to history listbox
+            showHistory();
+
+            // ensure that box is visible
+            historyListbox.Visible = true;
+        }
+
+        private void backPicturebox_Click(object sender, EventArgs e)
+        {
+            historyListbox.Visible = true;
+            showHistory();
+            string lastVisited = siteHistory.Peek();
+
+            int placementForLastVisited = historyListbox.Items.IndexOf(lastVisited);
+
+            historyListbox.SelectedIndex = placementForLastVisited;
+            historyListbox.TopIndex = placementForLastVisited;
+        }
+
+        private void showHistory()
+        {
+            historyListbox.Items.Clear();
+            foreach (string site in siteHistory)
+            {
+                historyListbox.Items.Add(site);
+            }
         }
     }
 }
