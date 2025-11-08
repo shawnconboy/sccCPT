ab9a629828af7956fde3385617499abdfb98f80d
Shawn Conboy <conboysbusiness@gmail.com>
2025-11-08 01:35:11 -0500

    update

---
+++ CPT 185 Event Driven Programming/chapterNotes/chapter9/slotMachine/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace slotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            spinButton = new Button();
+            exitButton = new Button();
+            label1 = new Label();
+            fruitImageList = new ImageList(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(136, 113);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 183);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(325, 113);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(150, 183);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(511, 113);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(150, 183);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // spinButton
+            // 
+            spinButton.Location = new Point(245, 357);
+            spinButton.Name = "spinButton";
+            spinButton.Size = new Size(112, 34);
+            spinButton.TabIndex = 1;
+            spinButton.Text = "Spin";
+            spinButton.UseVisualStyleBackColor = true;
+            spinButton.Click += spinButton_Click;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(439, 357);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(112, 34);
+            exitButton.TabIndex = 2;
+            exitButton.Text = "Exit";
+            exitButton.UseVisualStyleBackColor = true;
+            exitButton.Click += exitButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(347, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Spin To Win";
+            // 
+            // fruitImageList
+            // 
+            fruitImageList.ColorDepth = ColorDepth.Depth32Bit;
+            fruitImageList.ImageStream = (ImageListStreamer)resources.GetObject("fruitImageList.ImageStream");
+            fruitImageList.TransparentColor = Color.Transparent;
+            fruitImageList.Images.SetKeyName(0, "Apple.bmp");
+            fruitImageList.Images.SetKeyName(1, "Banana.bmp");
+            fruitImageList.Images.SetKeyName(2, "Cherries.bmp");
+            fruitImageList.Images.SetKeyName(3, "Grapes.bmp");
+            fruitImageList.Images.SetKeyName(4, "Lemon.bmp");
+            fruitImageList.Images.SetKeyName(5, "Lime.bmp");
+            fruitImageList.Images.SetKeyName(6, "Orange.bmp");
+            fruitImageList.Images.SetKeyName(7, "Pear.bmp");
+            fruitImageList.Images.SetKeyName(8, "Strawberry.bmp");
+            fruitImageList.Images.SetKeyName(9, "Watermelon.bmp");
+            // 
+            // Form1
+            // 
+            AcceptButton = spinButton;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = exitButton;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(exitButton);
+            Controls.Add(spinButton);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Spin To Win";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button spinButton;
+        private Button exitButton;
+        private Label label1;
+        private ImageList fruitImageList;
+    }
+}
---
+++ CPT 185 Event Driven Programming/chapterNotes/chapter9/slotMachine/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using Timer = System.Windows.Forms.Timer;
+
+namespace slotMachine
+{
+    public partial class Form1 : Form
+    {
+        private readonly Random rand = new Random();
+
+        // --- SPIN ANIMATION STATE ---
+        private readonly Timer spinTimer = new Timer(); // WinForms timer
+        private int ticks;
+        private int stopTickReel1, stopTickReel2, stopTickReel3;
+        private int idx1, idx2, idx3; // last shown indexes
+        private bool isSpinning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Timer ~20 frames/sec
+            spinTimer.Interval = 50;
+            spinTimer.Tick += SpinTimer_Tick;
+
+            // (Optional) nicer scaling
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Show an initial random frame
+            var _ = loadImages();
+        }
+
+        // Picks three random images, sets the picture boxes, and returns the indexes
+        private int[] loadImages()
+        {
+            int img1 = rand.Next(fruitImageList.Images.Count);
+            int img2 = rand.Next(fruitImageList.Images.Count);
+            int img3 = rand.Next(fruitImageList.Images.Count);
+
+            pictureBox1.Image = fruitImageList.Images[img1];
+            pictureBox2.Image = fruitImageList.Images[img2];
+            pictureBox3.Image = fruitImageList.Images[img3];
+
+            // Track last shown indexes (used by animation end + scoring)
+            idx1 = img1; idx2 = img2; idx3 = img3;
+
+            return new int[] { img1, img2, img3 };
+        }
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            if (isSpinning || fruitImageList.Images.Count < 3) return;
+
+            isSpinning = true;
+            spinButton.Enabled = false;
+            ticks = 0;
+
+            // Staggered stop times (feel free to tweak)
+            stopTickReel1 = 18 + rand.Next(6);                    // ~1.0s
+            stopTickReel2 = stopTickReel1 + 10 + rand.Next(6);    // ~1.5s
+            stopTickReel3 = stopTickReel2 + 10 + rand.Next(6);    // ~2.0s
+
+            spinTimer.Start();
+        }
+
+        private void SpinTimer_Tick(object sender, EventArgs e)
+        {
+            ticks++;
+
+            // Advance each reel until its stop tick
+            if (ticks <= stopTickReel1)
+            {
+                idx1 = rand.Next(fruitImageList.Images.Count);
+                pictureBox1.Image = fruitImageList.Images[idx1];
+            }
+            if (ticks <= stopTickReel2)
+            {
+                idx2 = rand.Next(fruitImageList.Images.Count);
+                pictureBox2.Image = fruitImageList.Images[idx2];
+            }
+            if (ticks <= stopTickReel3)
+            {
+                idx3 = rand.Next(fruitImageList.Images.Count);
+                pictureBox3.Image = fruitImageList.Images[idx3];
+            }
+
+            // All reels have passed their stop tick → stop and score
+            if (ticks > stopTickReel3)
+            {
+                spinTimer.Stop();
+                isSpinning = false;
+                spinButton.Enabled = true;
+
+                EvaluateResult(idx1, idx2, idx3);
+            }
+        }
+
+        private void EvaluateResult(int a, int b, int c)
+        {
+            if (a == b && b == c)
+            {
+                MessageBox.Show("JACKPOT! All three match!");
+            }
+            else if (a == b || a == c || b == c)
+            {
+                MessageBox.Show("Two of a kind! Small win!");
+            }
+            else
+            {
+                MessageBox.Show("No match. Try again!");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
