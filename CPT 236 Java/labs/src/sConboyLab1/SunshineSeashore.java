2011b43e4a826afc717bed9a839f13522dfe1cb8
Shawn Conboy <conboysbusiness@gmail.com>
2026-01-15 12:51:17 -0500

    Update java project and labs

---
+++ CPT 236 Java/labs/src/sConboyLab1/SunshineSeashore.java
@@ -0,0 +1,15 @@
+package sConboyLab1;
+
+public class SunshineSeashore {
+
+	public static void main(String[] args) {
+		
+		System.out.println("******************************************************");
+		System.out.println("*                                                    *");
+		System.out.println("*     Sunsine Seashore makes it fun in the sun.      *");
+		System.out.println("*                                                    *");
+		System.out.println("******************************************************");
+
+	}
+
+}
