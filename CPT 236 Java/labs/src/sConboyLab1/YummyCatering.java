b209a19abac54f39591ceb02e30a8e6e8e329fa4
Shawn Conboy <conboysbusiness@gmail.com>
2026-01-17 13:11:32 -0500

    Update lab 1

--- CPT 236 Java/labs/src/sConboyLab1/YummyCatering.java
+++ CPT 236 Java/labs/src/sConboyLab1/YummyCatering.java
@@ -1,3 +1,7 @@
+// Shawn Conboy
+// CPT 206 AO1 something
+// Lab 1 Yummy Catering
+
 package sConboyLab1;
 
 import java.util.Scanner;
@@ -7,45 +11,55 @@ public class YummyCatering {
 	public static void main(String[] args) {
 		
 		// variables
-		int numberOfGuest = 0;
-		int ticketPrice = 35;
+		short numberOfGuest = 0;
+		final int TICKET_PRICE = 35;
 		char eventSize = ' ';
 		int eventPrice = 0;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("******************************************************");
-		System.out.println("*                                                    *");
-		System.out.println("*    Yummy makes the food that makes it a party.     *");
-		System.out.println("*                                                    *");
-		System.out.println("******************************************************");
-		System.out.println();
+		// slogan
+		System.out.println("******************************************************\n" +
+						   "*                                                    *\n" +
+						   "*    Yummy makes the food that makes it a party.     *\n" +
+						   "*                                                    *\n" +
+						   "******************************************************\n");
 		
+		// program requirements
 		// prompt user for number of party guest.
 		// each guest is $35 a person.
 		// if 50 or more guests, event is "large".
 		
-		System.out.println("Hello. How many guests will be attending your event?");
-		numberOfGuest = input.nextInt();
+		// get user input
+		System.out.println("Welcome To Yummy Catering.\n" + 
+						   "How many guests will be attending your event?");
+		numberOfGuest = input.nextShort();
 		
-		// determine if event will be large
-		if (numberOfGuest >= 50) {
-			eventSize = 'L';
+		if (numberOfGuest <= 0) {
+			System.out.println("Sorry. We need at least 1 guest.");
 		} else {
-			eventSize = 'S';
+			// determine if event will be large
+			if (numberOfGuest >= 50) {
+				eventSize = 'L';
+			} else {
+				eventSize = 'S';
+			}
+			
+			// calculate total event price
+			eventPrice = numberOfGuest * TICKET_PRICE;
+			
+			// display to user
+			System.out.println("\nTotal Guests      = " + numberOfGuest +
+							   "\nPrice Per Guest   = $" + TICKET_PRICE +
+							   "\nTotal Event Price = $" + eventPrice);
+			
+			// display event size to user
+			if (eventSize == 'L') {
+				System.out.println("\nThis is a large event.");
+			} else {
+				System.out.println("\nThis is not a large event.");
+			}
 		}
 		
-		// calculate total event price
-		eventPrice = numberOfGuest * ticketPrice;
-		
-		// display to user
-		System.out.println("\nTotal Guests =  " + numberOfGuest +
-						   "\nPrice Per Guest = $" + ticketPrice +
-						   "\nTotal Event Price = $" + eventPrice);
-		
-		if (eventSize == 'L') {
-			System.out.println("This is a large event.");
-		} else {
-			System.out.println("This is not a large event.");
-		}
+		System.out.println("Program ended. Thank you.");
 	}
 }
