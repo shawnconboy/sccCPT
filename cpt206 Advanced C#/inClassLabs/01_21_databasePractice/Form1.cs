4080c7e6ba3ad23cbdf7d95badaa817a439868ba
Shawn Conboy <conboysbusiness@gmail.com>
2026-01-22 09:54:38 -0500

    update files

---
+++ cpt206 Advanced C#/inClassLabs/01_21_databasePractice/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_21_databasePractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.phonelistDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'phonelistDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.phonelistDataSet.Person);
+
+        }
+    }
+}
