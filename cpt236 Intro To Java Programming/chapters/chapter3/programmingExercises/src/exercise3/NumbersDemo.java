cb5f28b7eb7382851d4e824d08d6aa2d51fca878
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-02 11:06:56 -0500

    Worked on some java stuff

---
+++ cpt236 Intro To Java Programming/chapters/chapter3/programmingExercises/src/exercise3/NumbersDemo.java
@@ -0,0 +1,14 @@
+package exercise3;
+
+public class NumbersDemo {
+
+	public static void main(String[] args)
+	{
+		// get two numbers from user.
+		getNumbers();
+	}
+	
+	public static void getNumbers()
+	{
+	}
+}
