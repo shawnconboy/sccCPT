52122bebef5677e330639dac8f11e19aa8fb8f2a
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-02 09:59:49 -0500

    Update

---
+++ cpt206 Advanced C#/chapters/chapter4/calculatingFactorialsWithRecursion/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace calculatingFactorialsWithRecursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Factorial(int number)
+            {
+                if (number < 1)
+                {
+                    return 0;
+                } else if (number == 1)
+                {
+                    return 1;
+                } else
+                {
+                    checked
+                    {
+                        return number * Factorial(number - 1);
+                    }
+                }
+            }
+
+            static void RunFactorial()
+            {
+                for (int i = 1; i < 15; i++)
+                {
+                    try
+                    {
+                        Console.WriteLine($"{i}! = {Factorial(i):N0}");
+
+                    }
+                    catch (System.OverflowException)
+                    {
+                        Console.WriteLine($"{i}! is too big for a 32 bit-integer.");
+                    }
+                }
+            }
+            RunFactorial();
+        }
+    }
+}
