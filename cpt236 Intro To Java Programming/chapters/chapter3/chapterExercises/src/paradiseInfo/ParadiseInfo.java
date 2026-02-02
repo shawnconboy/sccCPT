f32a046ec30b3fdd58c7d7ca0c800e7922b4715e
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-02 11:14:44 -0500

    Random push

--- cpt236 Intro To Java Programming/chapters/chapter3/chapterExercises/src/paradiseInfo/ParadiseInfo.java
+++ cpt236 Intro To Java Programming/chapters/chapter3/chapterExercises/src/paradiseInfo/ParadiseInfo.java
@@ -2,14 +2,17 @@ package paradiseInfo;
 
 public class ParadiseInfo {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		displayInfo();
+		doSomething();
 	}
-	
-	public static void displayInfo()
-	{
+
+	public static void displayInfo() {
 		System.out.println("Paradise Day Spa wants to pamper you.");
 		System.out.println("We will make you look good, shawty.");
+	}
+
+	public static void doSomething() {
+		System.out.println("This is where we will be doing all the fun shit dawg.");
 	}
 }
