177650747f32b53523382dba746357a8f57560e9
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-25 03:25:08 -0500

    Java updates

--- cpt236 Intro To Java Programming/sConboyLab4/src/sConboyLab4/TextProcessor.java
+++ cpt236 Intro To Java Programming/sConboyLab4/src/sConboyLab4/TextProcessor.java
@@ -3,6 +3,19 @@ package sConboyLab4;
 public class TextProcessor 
 {
 
+	private StringBuilder currentText = new StringBuilder("");
+	
+	
+	// not super sure of what this class should do....?
+	// i'm imagining a class that holds methods to further
+	// extenuate the already existing StringBuilder methods....?
+	
+	// append text to an existing text.
+	public StringBuilder appendText(StringBuilder userInput)
+	{
+		return currentText;
+	}
+	
 	
 	
 }
---
+++ cpt236 Intro To Java Programming/sConboyLab4/src/sConboyLab4/TextProcessorMain.java
@@ -0,0 +1,12 @@
+package sConboyLab4;
+
+public class TextProcessorMain {
+
+	public static void main(String[] args) 
+	{
+	
+		// code will begin here
+
+	}
+
+}
