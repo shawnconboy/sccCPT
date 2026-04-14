23e944e79e9f1f05bb64191da27ff31e9680dfe0
Shawn Conboy <conboysbusiness@gmail.com>
2026-04-13 22:19:14 -0400

    update java

---
+++ cpt236 Intro To Java Programming/sConboyLab9/src/MainDemo.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class MainDemo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MainDemo
+{
+
+    public static void main(String[] args)
+    {
+        
+        Picture newPic = new Picture();
+        newPic.animate();
+        Person bob = new Person();
+        bob.makeVisible();
+        
+    }
+}
